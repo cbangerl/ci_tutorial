796f51fe2a72cf478ca1338a65018aacfefcc931
Chris Banner <chrisbanner87@gmail.com>
2020-06-06 20:58:28 +0200

    External test data for Tests

--- tests/test_myfun.py
+++ tests/test_myfun.py
@@ -1,18 +1,20 @@
 import numpy as np
 from myfun import mysum
 from myfun import myprod
+import re
+
+
 
 def test_mysum():
-    testData = np.arange(2,6)
-    resultSum = 14
+    testFile = open("./tests/testData.txt", "r")
+    fileString = testFile.readlines()
+    numbersString = fileString[0]
+    testString = re.sub("[^0-9,.]","",numbersString).split(",")
+    testData=[float(item) for item in testString]
+    testData = np.array(testData)
+    resultSum = float(re.sub("[^0-9.,]","",fileString[1]))
 
 
     testSum = mysum(testData)
     assert testSum  == resultSum
 
-def test_myprod():
-    testData = np.arange(2, 5)
-    resultProd = 24
-
-    testProd = myprod(testData)
-    assert testProd == resultProd
